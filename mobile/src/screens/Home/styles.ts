d12f2a02c78e984b5ac773bf8e5115af4e364bff
Caio Victor <caiosarmento@eng.ci.ufpb.br>
2022-09-14 01:07:45 -0300

    creating mobile landing page

---
+++ mobile/src/screens/Home/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  logo: {
+    width: 214,
+    height: 120,
+    marginTop: 74,
+    marginBottom: 48,
+  },
+  contentList: {
+    paddingHorizontal: 32,
+  }
+});
